e319566804502c578c6b1b46df15aec194370eae
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-06 15:06:37 -0400

    week 1 class 2 added Brand and Product domain classes

---
+++ CaseStudyBooksAPI/DAL/DomainClasses/Brand.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CaseStudyBooksAPI.DAL.DomainClasses
+{
+    /**
+     * We are trying to sell products. Each product has a brand.
+     * This class represents the brands (publishers).
+     */
+    public class Brand
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "timestamp")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [MaxLength(8)]
+        public byte[] Timer { get; set; }
+    }
+}
---
+++ CaseStudyBooksAPI/DAL/DomainClasses/Product.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CaseStudyBooksAPI.DAL.DomainClasses
+{
+    /**
+     * This class models a product we sell.
+     */ 
+    public class Product
+    {
+        // Primary Key
+        [Key]
+        [Required]
+        public string ProductName;
+
+        // Brand [1] - Product [N]
+        // Foreign Key
+        [Required]
+        public int BrandId { get; set; }
+        [ForeignKey("BrandId")]
+        public Brand Brand { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        [Range(0, 9999.99)]
+        public decimal CostPrice { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        [Range(0, 9999.99)]
+        public decimal MSRP { get; set; }
+
+        [Required]
+        public int QtyOnHand { get; set; }
+
+        [Required]
+        public int QtyOnBackOrder { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public int ReleasedYear { get; set; }
+
+        [Column(TypeName = "timestamp")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [MaxLength(8)]
+        public byte[] Timer { get; set; }
+    }
+}
