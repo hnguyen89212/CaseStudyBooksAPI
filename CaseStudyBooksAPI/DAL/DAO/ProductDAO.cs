cb9719b6fec3dc725532e74cde13cd905422921d
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-10 20:13:25 -0400

    week 2 class 1 added ProductDAO and ProductController getAll

---
+++ CaseStudyBooksAPI/DAL/DAO/ProductDAO.cs
@@ -0,0 +1,27 @@
+﻿using CaseStudyBooksAPI.DAL.DomainClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseStudyBooksAPI.DAL.DAO
+{
+    /**
+     * Product repository class to access DB through db context.
+     */ 
+    public class ProductDAO
+    {
+        private AppDbContext _db;
+
+        public ProductDAO(AppDbContext context)
+        {
+            _db = context;
+        }
+
+        // Gets all products of a brand.
+        public List<Product> GetAllByBrand(int brandId)
+        {
+            return _db.Products.Where(each => each.BrandId == brandId).ToList();
+        }
+    }
+}
