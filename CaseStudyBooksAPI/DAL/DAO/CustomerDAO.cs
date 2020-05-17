6d40c3a1c86428f7309f9430427b930228382a50
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-17 19:13:59 -0400

    added CustomerDAO

---
+++ CaseStudyBooksAPI/DAL/DAO/CustomerDAO.cs
@@ -0,0 +1,30 @@
+﻿using CaseStudyBooksAPI.DAL.DomainClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseStudyBooksAPI.DAL.DAO
+{
+    public class CustomerDAO
+    {
+        private AppDbContext _db;
+
+        public CustomerDAO(AppDbContext context)
+        {
+            _db = context;
+        }
+
+        public Customer Register(Customer user)
+        {
+            _db.Customers.Add(user);
+            _db.SaveChanges();
+            return user;
+        }
+
+        public Customer GetByEmail(string email)
+        {
+            return _db.Customers.FirstOrDefault(each => each.Email == email);
+        }
+    }
+}
