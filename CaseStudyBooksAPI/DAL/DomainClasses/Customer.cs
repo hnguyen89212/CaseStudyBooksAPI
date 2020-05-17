dc5cb3302be1d88cd5471eb86d031ee65f2ed8e5
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-17 18:35:36 -0400

    added Customer domain class

---
+++ CaseStudyBooksAPI/DAL/DomainClasses/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseStudyBooksAPI.DAL.DomainClasses
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Hash { get; set; }
+
+        [Required]
+        public string Salt { get; set; }
+    }
+}
