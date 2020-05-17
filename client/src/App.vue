13746df1b148d9a6559b864dae9aa8556c2bf572
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-17 10:26:07 -0400

    added color schemes, toolbar and footer for App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <v-app>
     <v-content>
+      <v-toolbar color="primary" class="white--text">
+        <v-toolbar-title>
+          <span>Info3067Exercises</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
       <BrandList />
+      <v-footer absolute class="headline" color="warning">
+        <v-col class="text-center" cols="12">
+          &copy;{{ new Date().getFullYear() }} — INFO3067
+        </v-col>
+      </v-footer>
     </v-content>
   </v-app>
 </template>
