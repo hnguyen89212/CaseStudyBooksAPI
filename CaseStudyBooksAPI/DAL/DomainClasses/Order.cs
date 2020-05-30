5bfc08150c21b861d49cc936318987e698373817
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-30 11:46:22 -0400

    added Order domain class

---
+++ CaseStudyBooksAPI/DAL/DomainClasses/Order.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseStudyBooksAPI.DAL.DomainClasses
+{
+    /**
+     * A class to represent an order and capture:
+     * - Id of order
+     * - date time of placement
+     * - total amount of money
+     * - customer identity
+     * - collection of the ordered items.
+     */ 
+    public class Order
+    {
+        public Order()
+        {
+            OrderLineItems = new HashSet<OrderLineItem>();
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal OrderAmount { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public int CustomerId { get; set; }
+
+        public virtual ICollection<OrderLineItem> OrderLineItems { get; set; }
+    }
+}
