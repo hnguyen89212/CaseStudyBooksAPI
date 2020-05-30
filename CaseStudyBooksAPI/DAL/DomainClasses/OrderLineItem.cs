79ad1dcd143ffb0263538af1786de1be2961de52
Hai Nguyen (Kevin) <54988182+kevin-hnguyen@users.noreply.github.com>
2020-05-30 11:54:46 -0400

    added OrderLineItem class

---
+++ CaseStudyBooksAPI/DAL/DomainClasses/OrderLineItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaseStudyBooksAPI.DAL.DomainClasses
+{
+    /**
+     * A class to model an item in an order.
+     * It should be distinguished from a product.
+     */ 
+    public class OrderLineItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        public int QtyOrdered { get; set; }
+
+        public int QtySold { get; set; }
+
+        public int QtyBackOrdered { get; set; }
+    }
+}
